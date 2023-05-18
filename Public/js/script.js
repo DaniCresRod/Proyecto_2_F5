f2b52a65dd777c4ed15fa4dc29e62ec8f98b70f2
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-05-18 23:00:12 +0200

    cambios en casa de ayer

--- Public/js/script.js
+++ Public/js/script.js
@@ -2,9 +2,9 @@
 var palabra_extra=document.querySelector("nav ul :nth-child(4)");
 
 palabra_extra.addEventListener("mouseover", function(e){
-    document.getElementById("menu_arrow").src="/images/down-arrow-red.png";
+    document.getElementById("menu_arrow").src="./images/down-arrow-red.png";
 })
 
 palabra_extra.addEventListener("mouseout", function(e){
-    document.getElementById("menu_arrow").src="/images/down-arrow.png";
+    document.getElementById("menu_arrow").src="./images/down-arrow.png";
 })
