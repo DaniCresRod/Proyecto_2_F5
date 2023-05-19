5363704a8e3cc5fda040e8e9ef8cb9be687e6e5b
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-05-19 14:57:03 +0200

    Funciona como debe

--- Public/js/script.js
+++ Public/js/script.js
@@ -8,3 +8,41 @@ palabra_extra.addEventListener("mouseover", function(e){
 palabra_extra.addEventListener("mouseout", function(e){
     document.getElementById("menu_arrow").src="./images/down-arrow.png";
 })
+
+//Cambia la imagen del boton a blanco
+var icono_reloj=document.querySelector("#tab_features div :nth-child(1)");
+var icono_hamburguesa=document.querySelector("#tab_features div :nth-child(2)");
+var icono_calendario=document.querySelector("#tab_features div :nth-child(3)");
+
+icono_reloj.addEventListener("mouseover", function(e){
+
+    icono_reloj.querySelector("img").src="./images/reloj.png";
+});
+
+icono_reloj.addEventListener("mouseout", function(e){
+
+    icono_reloj.querySelector("img").src="./images/reloj_2.png";
+});
+
+icono_hamburguesa.addEventListener("mouseover", function(e){
+
+    icono_hamburguesa.querySelector("img").src="./images/menu_hamb.png";
+});
+
+icono_hamburguesa.addEventListener("mouseout", function(e){
+
+    icono_hamburguesa.querySelector("img").src="./images/menu_hamb_2.png";
+});
+
+icono_calendario.addEventListener("mouseover", function(e){
+
+    icono_calendario.querySelector("img").src="./images/calendario.png";
+});
+
+icono_calendario.addEventListener("mouseout", function(e){
+
+    icono_calendario.querySelector("img").src="./images/calendario_2.png";
+});
+
+
+
